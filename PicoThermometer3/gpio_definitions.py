42237e58d49c2d8846339f80b32b72e83210f7ca
Jaroslav Dvorak <56601456+Jaroslav-Dvorak@users.noreply.github.com>
2023-08-30 21:55:55 +0200

    Color e-ink

---
+++ PicoThermometer3/gpio_definitions.py
@@ -0,0 +1,35 @@
+from machine import Pin, ADC
+from machine import SPI as Spi
+
+# display WeAct
+# 1 Busy    purple
+# 2 Res     orange
+# 3 D/C     white
+# 4 CS      blue
+# 5 SCL     green
+# 6 SDA     yellow
+# 7 GND     black
+# 8 VCC     red
+BUSY_PIN = Pin(10, Pin.IN, Pin.PULL_UP)
+RST_PIN = Pin(11, Pin.OUT)
+DC_PIN = Pin(12, Pin.OUT)
+CS_PIN = Pin(13, Pin.OUT)
+SDA_PIN = Pin(15)  # SDA=MOSI
+SCL_PIN = Pin(14)  # SCL=SCK
+SPI_NUM = 1
+SPI = Spi(SPI_NUM, baudrate=1_000_000, sck=SCL_PIN, mosi=SDA_PIN)
+
+# tp5110
+DONE_PIN = Pin(22, Pin.OUT)
+
+# analog
+BATT_ADC = ADC(28)
+TEMPER_ADC = ADC(4)
+
+# dallas
+# TEMP_SENS_1 = Pin(0)
+# TEMP_SENS_2 = Pin(1)
+# TEMP_SENS_3 = Pin(2)
+
+# led
+LED = Pin('LED', Pin.OUT)
---
+++ PicoThermometer3/main.py
@@ -0,0 +1,55 @@
+from gdey0213z98 import GDEY0213Z98
+# from pict import *
+from time import sleep_ms
+from gpio_definitions import *
+from vizualizer import Vizualizer, BLACK, WHITE, RED
+import nonvolatile
+import measurement
+
+background = WHITE
+viz = Vizualizer(background)
+
+Left = 0
+Right = 249
+Top = 121
+Bottom = 0
+
+# viz.tiny_text("zdar zdar zdarau cus", Left, Top - 20, BLACK)
+# viz.tiny_text("CHRRRRRST", Left, Bottom + 8, 2)
+#
+# viz.line(Left, Bottom, Right, Bottom, RED)
+# viz.line(Left, Top, Left, Bottom, RED)
+# viz.line(Right, Top, Right, Bottom, RED)
+# viz.line(Left, Top, Right, Top, RED)
+# #
+# viz.line(Left, Top, Right, Bottom, 2)
+# viz.line(Left, Bottom, Right, Top, 2)
+
+# viz.rect(66, 80, 50, 70)
+# viz.fill_rect(140, 33, 50, 70, 2)
+# viz.large_text("21.3", 0, 0, BLACK)
+# viz.canvas_black.text("zdar jak pes", 0, 0)
+# viz.canvas_red.text("RED RED RED", 0, 30)
+
+# epd_hw_init(rotation=False)   # Full screen refresh initialization.
+# EPD_WhiteScreen_ALL(gImage_BW1, gImage_RW1)     # To Display one image using full screen refresh.
+
+while True:
+    temper_onboard_voltage = measurement.measure_analog(TEMPER_ADC)
+    onboard_temperature = 27 - (temper_onboard_voltage - 0.706) / 0.001721
+    optimized_temp = int((onboard_temperature - 25) * 10)
+    if optimized_temp < -127:
+        optimized_temp = -127
+    elif optimized_temp > 128:
+        optimized_temp = 128
+    temperatures = nonvolatile.save_and_get_last_values(optimized_temp, "temperatures.dat")
+
+    temperatures = [temp / 10 + 25 for temp in temperatures]
+    viz.chart(temperatures, minimum=15, maximum=35)
+
+    eink = GDEY0213Z98(busy=BUSY_PIN, rst=RST_PIN, dc=DC_PIN, cs=CS_PIN, spi=SPI, border=background)
+    # eink.show_straight(gImage_BW1, gImage_RW1)     # To Display one image using full screen refresh.
+    eink.show(viz.buffer_black, viz.buffer_red)     # To Display one image using full screen refresh.
+
+    eink.deep_sleep()     # Enter the sleep mode and please do not delete it, otherwise it will reduce the lifespan of the screen.\
+    sleep_ms(10000)
