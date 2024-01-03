6d7e781254e64b76a32c5a734c821e84feb6294a
Yarda89 <56601456+Yarda89@users.noreply.github.com>
2024-01-03 05:47:39 +0100

    SHT4x entry

--- PicoThermometer/sensor.py
+++ PicoThermometer/sensor.py
@@ -7,14 +7,14 @@
 
 # _____________________SCD4X_____________________________________________________
 
-from machine import I2C, Pin
-from sensors.scd4x import SCD4X
-
-Pin(8, Pin.OUT).value(1)
-Pin(9, Pin.OUT).value(0)
-
-I2c = I2C(1, scl=Pin(7), sda=Pin(6), freq=400_000)
-sensor = SCD4X(I2c)
+# from machine import I2C, Pin
+# from sensors.scd4x import SCD4X
+#
+# Pin(8, Pin.OUT).value(1)
+# Pin(9, Pin.OUT).value(0)
+#
+# I2c = I2C(1, scl=Pin(7), sda=Pin(6), freq=400_000)
+# sensor = SCD4X(I2c)
 
 # _______________________SOIL MOISTURE_________________________________________)
 
@@ -36,3 +36,14 @@ sensor = SCD4X(I2c)
 # dht_data = Pin(8, Pin.IN, Pin.PULL_UP)
 # sleep_ms(1100)
 # sensor = PicoDHT22(dataPin=dht_data, filename="sensors/dht22_1.json")
+
+# _____________________SCD4X_____________________________________________________
+
+from machine import I2C, Pin
+from sensors.sht4x import SHT4X
+
+Pin(8, Pin.OUT).value(0)
+Pin(9, Pin.OUT).value(9)
+
+I2c = I2C(1, scl=Pin(7), sda=Pin(6), freq=400_000)
+sensor = SHT4X(I2c)
